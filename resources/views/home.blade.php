b9d2260a4ef5f5136d77930ce826b1b1f6e833f4
root <ritesh.bisht93@gamil.com>
2015-07-13 23:39:21 +0530

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,93 @@
-@extends('app')
+<html>
 
-@section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+<head>
+  <title>Home</title>
 
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
-	</div>
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Carter+One">
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Aldrich">
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+<style>
+html {
+  font-family: 'Aldrich';
+  color:white;
+}
+
+</style>
+</head>
+<body style="background:black;" >
+
+  <div class="navbar-fixed" >
+    <nav style="background:none;box-shadow:none;">
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo center"></a>
+        <ul class="left hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li class="active"><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+<div class="conatiner-fluid" style="width:inherit;height:80%;background:url('images/back.jpg');background-size:100% 100%;background-repeat:no-repeat;" >
+        <div class="container-fluid" style="position:relative;top:10%;">
+                    <div class="row  " style="height:300px;">
+
+
+
+
+
+                                    <div class="col s12 m12 l12 center-align" style="height:inherit;">
+                                                <div class="row centre-align">
+                                                      <h1 style="width:inherit;">
+                                                        <p>WEBSITE NAME</p>
+                                                        <p > A TAG LINE</p></h1>
+
+                                                </div>
+
+
+                                                <div class="row ">
+                                                    <div class="col s12 m4">
+                                                    <img class="circle" src="images/notes.jpg"/>
+                                                    <h4>NOTES</h4>
+                                                  </div>
+
+
+                                                      <div class="col s12 m4">
+                                                      <img class="circle" src="images/notes.jpg"/>
+                                                      <h4>NOTES</h4>
+                                                    </div>
+
+
+                                                        <div class="col s12 m4">
+                                                        <img class="circle" src="images/notes.jpg"/>
+                                                        <h4>NOTES</h4>
+                                                      </div>
+                                                </div>
+
+
+
+                                    </div>
+
+
+
+
+
+
+                     </div>
+
+                     <div class="row center " >
+
+                    </div>
+
+        </div>
+
 </div>
-@endsection
+</body>
+
+
+
+</html>
