f63b09aabf3b2852030aa4571a8ca439105bd055
riteshbisht <ritesh.bisht93@gamil.com>
2015-07-24 16:47:47 +0530

    fixed the login with facebook

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,17 +30,13 @@
 				<ul class="left hide-on-med-and-down">
 					<li class="active"><a href="{{url('/')}}" class="black-text">HOME</a></li>
 					<li><a href="collapsible.html" class="black-text">CONTACT</a></li>
-					@if (Auth::guest())
-					<li><a href="{{ url('/auth/login') }}">Login</a></li>
-					<li><a href="{{ url('/auth/register') }}">Register</a></li>
-					@else
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 						<ul class="dropdown-menu" role="menu">
 							<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
 						</ul>
 					</li>
-					@endif
+					
 				</ul>
 			</div>
 		</nav>
