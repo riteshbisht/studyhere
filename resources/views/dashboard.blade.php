97a509dc58b01fe75da06c550937cf807878b76c
riteshbisht <ritesh.bisht93@gamil.com>
2015-10-02 12:08:54 +0530

    added https in stead http in cdn

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
   <title>Tutelage</title>
 
   <!-- CSS  -->
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
   <link rel="stylesheet" href="css//signin/sb-admin-2.css">
   <link rel="stylesheet" href="css/signin/dash.css">
@@ -20,7 +20,7 @@
   <!-- BEGIN GLOBAL MANDATORY STYLES -->
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <script src="js/signin/materialize.js"></script>
   <script src="js/signin/materialize.min.js"></script>
   <script src="js/signin/dash_js.js"></script>
